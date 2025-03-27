37fdb71715af1aec30e25e4401d35d2a13b535e0
rn-mgl <reinb2ra@gmail.com>
2025-03-27 21:40:26 +0800

    cleanup

--- controllers/head/auth/authHeadController.js
+++ controllers/head/auth/authHeadController.js
@@ -10,29 +10,39 @@ const verifyHead = async (req, res) => {
   const { token } = req.body;
 
   if (!token) {
-    throw new BadRequestError(`You do not have the appropriate credentials to verify your account.`);
+    throw new BadRequestError(
+      `You do not have the appropriate credentials to verify your account.`
+    );
   }
 
   const isExpired = fns.isTokenExpired(token);
 
   if (isExpired) {
-    throw new BadRequestError(`The link for changing the passord has already expired.`);
+    throw new BadRequestError(
+      `The link for changing the passord has already expired.`
+    );
   }
 
   const verify = jwt.verify(token, process.env.JWT_SECRET);
 
   if (!verify) {
-    throw new BadRequestError(`You do not have the appropriate credentials to verify your account.`);
+    throw new BadRequestError(
+      `You do not have the appropriate credentials to verify your account.`
+    );
   }
 
   if (!verify?.id || !verify?.username || !verify?.email || !verify?.role) {
-    throw new BadRequestError(`You do not have the appropriate credentials to verify your account.`);
+    throw new BadRequestError(
+      `You do not have the appropriate credentials to verify your account.`
+    );
   }
 
   const head = await Head.verifyHead(verify.id);
 
   if (!head) {
-    throw new BadRequestError(`Error in verifying your account. Try again later.`);
+    throw new BadRequestError(
+      `Error in verifying your account. Try again later.`
+    );
   }
 
   res.status(StatusCodes.OK).json(head);
@@ -55,7 +65,8 @@ const logInHead = async (req, res) => {
     throw new NotFoundError(`There is no user with the given email.`);
   }
 
-  const { head_id, name, surname, username, email, password, is_verified } = head;
+  const { head_id, name, surname, username, email, password, is_verified } =
+    head;
 
   const isMatch = await fns.isMatchedPassword(password, candidatePassword);
 
@@ -81,7 +92,11 @@ const logInHead = async (req, res) => {
 
     res.status(StatusCodes.OK).json({ primary });
 
-    const mail = await sendVerificationEmail(email, `${name} ${surname}`, token);
+    const mail = await sendVerificationEmail(
+      email,
+      `${name} ${surname}`,
+      token
+    );
     return;
   } else {
     const token = fns.createLogInToken(head_id, username, email, "head");
@@ -93,38 +108,4 @@ const logInHead = async (req, res) => {
   }
 };
 
-const signUpHead = async (req, res) => {
-  const { userData } = req.body;
-
-  const { name, surname, username, email, password, image } = userData;
-
-  const takenUsername = await Head.findWithUsername(email);
-
-  if (takenUsername) {
-    throw new BadRequestError(`The username ${username} has already been taken.`);
-  }
-
-  const takenEmail = await Head.findWithEmail(email);
-
-  if (takenEmail) {
-    throw new BadRequestError(`The email ${email} is already used in Readefine.`);
-  }
-
-  const hashedPassword = await fns.hashPassword(password);
-
-  const head = new Head(name, surname, username, email, hashedPassword, image);
-
-  const data = await head.createHead();
-
-  if (!data) {
-    throw new BadRequestError(`Error in signing up. Try again later.`);
-  }
-
-  const token = fns.createSignUpToken(data.insertId, username, email, "head");
-
-  res.status(StatusCodes.OK).json({ data, token });
-
-  const mail = await sendVerificationEmail(email, `${name} ${surname}`, token);
-};
-
-module.exports = { logInHead, signUpHead, verifyHead };
+module.exports = { logInHead, verifyHead };
--- routers/head/auth/authHeadRouter.js
+++ routers/head/auth/authHeadRouter.js
@@ -4,6 +4,5 @@ const controller = require("../../../controllers/head/auth/authHeadController");
 
 router.route("/head_verify").patch(controller.verifyHead);
 router.route("/head_login").post(controller.logInHead);
-router.route("/head_signup").post(controller.signUpHead);
 
 module.exports = router;
